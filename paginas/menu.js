9527190af9ddfbbd71332071eccb2c61b998416b
Tavio8 <123322021+Tavio8@users.noreply.github.com>
2023-04-22 20:20:42 -0300

    atualização

---
+++ paginas/menu.js
@@ -0,0 +1,80 @@
+//NATUREZA----------------------------------------------------------------------
+//Campo-------------------------------------------------------------------------
+function temaAleatorioCampo(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-campo/campo-${num}.html`;
+}
+function campo(){
+    location.href = temaAleatorioCampo();
+}
+
+//Floresta----------------------------------------------------------------------
+
+function temaAleatorioFloresta(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-floresta/floresta-${num}.html`;
+}
+function floresta(){
+    location.href = temaAleatorioFloresta();
+}
+
+//Montanhas----------------------------------------------------------------------
+
+function temaAleatorioMontanhas(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-montanhas/montanhas-${num}.html`;
+}
+function montanhas(){
+    location.href = temaAleatorioMontanhas();
+}
+
+//Noite----------------------------------------------------------------------
+
+function temaAleatorioNoite(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-noite/noite-${num}.html`;
+}
+function noite(){
+    location.href = temaAleatorioNoite();
+}
+
+//Oceano----------------------------------------------------------------------
+
+function temaAleatorioOceano(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-oceano/oceano-${num}.html`;
+}
+function oceano(){
+    location.href = temaAleatorioOceano();
+}
+
+//Por-do-Sol----------------------------------------------------------------------
+
+function temaAleatorioPordSol(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-por-do-sol/por-do-sol-${num}.html`;
+}
+function pordsol(){
+    location.href = temaAleatorioPordSol();
+}
+
+//Tempestade----------------------------------------------------------------------
+
+function temaAleatorioTempestade(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-tempestade/tempestade-${num}.html`;
+}
+function tempestades(){
+    location.href = temaAleatorioTempestade();
+}
+
+//Universo----------------------------------------------------------------------
+
+function temaAleatorioUniverso(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Natureza/tema-universo/universo-${num}.html`;
+}
+function universo(){
+    location.href = temaAleatorioUniverso();
+}
+
