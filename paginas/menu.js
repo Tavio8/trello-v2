9be3501da0f6226b8254a0de697b1eb1aa0284af
Tavio8 <123322021+Tavio8@users.noreply.github.com>
2023-04-23 17:05:37 -0300

    atualização

--- paginas/menu.js
+++ paginas/menu.js
@@ -79,11 +79,255 @@ function universo(){
 }
 //NATUREZA-FIM-------------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+
 //ARTES----------------------------------------------------------------------
-function temaAleatorioCampo(){
+//Musica---------------------------------------------------------------------
+function temaAleatorioMusica(){
     const num = Math.floor(Math.random() * 5)+1;
-    return `Natureza/tema-campo/campo-${num}.html`;
+    return `Arte/tema-musica/musica-${num}.html`;
 }
-function campo(){
-    location.href = temaAleatorioCampo();
+function musica(){
+    location.href = temaAleatorioMusica();
 }
+
+//Pintura----------------------------------------------------------------------
+function temaAleatorioPintura(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-pintura/pintura-${num}.html`;
+}
+function pintura(){
+    location.href = temaAleatorioPintura();
+}
+
+//Escultura----------------------------------------------------------------------
+function temaAleatorioEscultura(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-escultura/escultura-${num}.html`;
+}
+function escultura(){
+    location.href = temaAleatorioEscultura();
+}
+
+//Teatro----------------------------------------------------------------------
+function temaAleatorioTeatro(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-teatro/teatro-${num}.html`;
+}
+function teatro(){
+    location.href = temaAleatorioTeatro();
+}
+
+//Dança----------------------------------------------------------------------
+function temaAleatorioDança(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-dança/dança-${num}.html`;
+}
+function dança(){
+    location.href = temaAleatorioDança();
+}
+
+//Literatura----------------------------------------------------------------------
+function temaAleatorioLiteratura(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-literatura/literatura-${num}.html`;
+}
+function literatura(){
+    location.href = temaAleatorioLiteratura();
+}
+
+//Cinema----------------------------------------------------------------------
+function temaAleatorioCinema(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-cinema/cinema-${num}.html`;
+}
+function cinema(){
+    location.href = temaAleatorioCinema();
+}
+
+//Fotografia----------------------------------------------------------------------
+function temaAleatorioFotografia(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Arte/tema-fotografia/fotografia-${num}.html`;
+}
+function fotografia(){
+    location.href = temaAleatorioFotografia();
+}
+
+//ARTES-FIM--------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+//INTERNET---------------------------------------------------------------------
+//Memes---------------------------------------------------------------------
+function temaAleatorioMemes(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-meme/meme-${num}.html`;
+}
+function memes(){
+    location.href = temaAleatorioMemes();
+}
+
+//Jogos----------------------------------------------------------------------
+function temaAleatorioJogos(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-jogo/jogo-${num}.html`;
+}
+function jogos(){
+    location.href = temaAleatorioJogos();
+}
+
+//Fantasia----------------------------------------------------------------------
+function temaAleatorioFantasia(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-fantasia/fantasia-${num}.html`;
+}
+function fantasia(){
+    location.href = temaAleatorioFantasia();
+}
+
+//Filmes----------------------------------------------------------------------
+function temaAleatorioFilmes(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-filmes/filmes-${num}.html`;
+}
+function filmes(){
+    location.href = temaAleatorioFilmes();
+}
+
+//Livros----------------------------------------------------------------------
+function temaAleatorioLivros(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-livro/livro-${num}.html`;
+}
+function livros(){
+    location.href = temaAleatorioLivros();
+}
+
+//Series----------------------------------------------------------------------
+function temaAleatorioSeries(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-series/series-${num}.html`;
+}
+function series(){
+    location.href = temaAleatorioSeries();
+}
+
+//Ficção----------------------------------------------------------------------
+function temaAleatorioFicção(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-ficção/ficção-${num}.html`;
+}
+function ficção(){
+    location.href = temaAleatorioFicção();
+}
+
+//Horror----------------------------------------------------------------------
+function temaAleatorioHorror(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Internet/tema-horror/horror-${num}.html`;
+}
+function horror(){
+    location.href = temaAleatorioHorror();
+}
+//INTERNET-FIM--------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+//ANIMAIS----------------------------------------------------------------------
+//Mamiferos----------------------------------------------------------------------
+function temaAleatorioMamiferos(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-mamiferos/mamiferos-${num}.html`;
+}
+function mamiferos(){
+    location.href = temaAleatorioMamiferos();
+}
+
+//Repteis----------------------------------------------------------------------
+function temaAleatorioRepteis(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-repteis/repteis-${num}.html`;
+}
+function repteis(){
+    location.href = temaAleatorioRepteis();
+}
+
+//Aves----------------------------------------------------------------------
+function temaAleatorioAves(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-aves/aves-${num}.html`;
+}
+function aves(){
+    location.href = temaAleatorioAves();
+}
+
+//Anfibios----------------------------------------------------------------------
+function temaAleatorioAnfibios(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-anfibios/anfibios-${num}.html`;
+}
+function anfibios(){
+    location.href = temaAleatorioAnfibios();
+}
+
+//Peixes----------------------------------------------------------------------
+function temaAleatorioPeixes(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-peixes/peixes-${num}.html`;
+}
+function peixes(){
+    location.href = temaAleatorioPeixes();
+}
+
+//Insetos----------------------------------------------------------------------
+function temaAleatorioInsetos(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-insetos/insetos-${num}.html`;
+}
+function insetos(){
+    location.href = temaAleatorioInsetos();
+}
+
+//Microorganismo----------------------------------------------------------------------
+function temaAleatorioMicroorganismo(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-microorganismo/microorganismo-${num}.html`;
+}
+function microorganismo(){
+    location.href = temaAleatorioMicroorganismo();
+}
+
+//Hibridos----------------------------------------------------------------------
+function temaAleatorioHibridos(){
+    const num = Math.floor(Math.random() * 5)+1;
+    return `Animais/tema-hibridos/hibridos-${num}.html`;
+}
+function hibridos(){
+    location.href = temaAleatorioHibridos();
+}
+
+//ANIMIAS-FIM--------------------------------------------------------------------
